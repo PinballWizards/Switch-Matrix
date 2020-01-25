acb0228191262d4271900ee7c44ef8887bf0b93d
jennybrewer <brewerjf@mail.uc.edu>
2020-01-25 15:03:02 -0500

    update to led checking

--- src/main.rs
+++ src/main.rs
@@ -31,55 +31,89 @@ fn main() -> ! {
     );
 
     let mut pins = hal::Pins::new(peripherals.PORT);
-    let mut column_one = pins.a1.into_open_drain_output(&mut pins.port);
-    let mut column_two = pins.a2.into_open_drain_output(&mut pins.port);
-    let mut column_three = pins.a3.into_open_drain_output(&mut pins.port);
-    let mut column_four = pins.a4.into_open_drain_output(&mut pins.port);
-    let mut column_five = pins.d10.into_open_drain_output(&mut pins.port);
+    let mut column_one = pins.a1.into_push_pull_output(&mut pins.port);
+    let mut column_two = pins.a2.into_push_pull_output(&mut pins.port);
+    let mut column_three = pins.a3.into_push_pull_output(&mut pins.port);
+    let mut column_four = pins.a4.into_push_pull_output(&mut pins.port);
+    let mut column_five = pins.d10.into_push_pull_output(&mut pins.port);
+    let mut led_one = pins.d13.into_push_pull_output(&mut pins.port);
+    let mut led_two = pins.a5.into_push_pull_output(&mut pins.port);
+    let mut led_three = pins.d5.into_push_pull_output(&mut pins.port);
+    let mut led_four = pins.d6.into_push_pull_output(&mut pins.port);
     let mut delay = Delay::new(core.SYST, &mut clocks);
-    let mut input_one = pins.d11.into_function_b(&mut pins.port);
-    let mut input_two = pins.d12.into_function_b(&mut pins.port);
+    let mut input_one = pins.d11.into_pull_down_input(&mut pins.port);
+    let mut input_two = pins.d12.into_pull_down_input(&mut pins.port);
 
     loop {
-       column_one.set_high().unwrap();     // set column 1 to high
-       if input_one  {                     // input 1 high
-            println!("Button 1 was pressed");
-       } else if input_two {               // input 2 high
-            println!("Button 6 was pressed");
-       } 
-       delay.delay_ms(200u8);
-       column_one.set_low().unwrap();      // set column 1 to low
-       column_two.set_high().unwrap();     // set column 2 to high 
-       if input_one {                      // input 1 high
-            println!("Button 2 was pressed");
-       } else if input_two {               // input 2 high
-            println!("Button 7 was pressed");
-       }
-       delay.delay_ms(200u8);
-       column_two.set_low().unwrap();      // set column 2 to low
-       column_three.set_high().unwrap();   // set column 3 to high
-       if input_one {                      // input 1 high
-            println!("Button 3 was pressed");
-       } else if input_two {               // input 2 high
-            println!("Button 8 was pressed");
-       }
-       delay.delay_ms(200u8);
-       column_three.set_low().unwrap();    // set column 3 to low
-       column_four.set_high().unwrap();    // set column 4 to high
-       if input_one {                      // input 1 high
-            println!("Button 4 was pressed");
-       } else if input_two {               // input 2 high
-            println!("Button 9 was pressed")'
-       }
-       delay.delay_ms(200u8);
-       column_four.set_low().unwrap();     // set column 4 to low
-       column_five.set_high().unwrap();    // set column 5 to high
-       if input_one {                      // input 1 high
-            println!("Button 5 was pressed");
-       } else if input_two {               // input 2 high
-            println!("Button 10 was pressed");
-       }
-       delay.delay_ms(200u8);
-       column_five.set_low().unwrap();     // set column 5 to low
-    }
+     //   column_one.set_high();     // set column 1 to high
+     //    if input_one.is_high()  {                     // input 1 high button 1
+             led_one.set_high();
+             led_two.set_low();
+             led_three.set_low();
+             led_four.set_low();
+     //   } else if input_two.is_high() {               // input 2 high button 6
+     //        led_three.set_high();
+     //       led_two.set_high();
+     //         led_one.set_low();
+     //         led_four.set_low();
+     //   } 
+        delay.delay_ms(200u8);
+     //   column_one.set_low();      // set column 1 to low
+     //   column_two.set_high();     // set column 2 to high
+     //   if input_one.is_high() {                      // input 1 high button 2
+             led_two.set_high();
+              led_one.set_low();
+              led_three.set_low();
+              led_four.set_low();
+     //   } else if input_two.is_high() {               // input 2 high button 7
+     //        led_one.set_high();
+     //         led_two.set_high();
+     //         led_three.set_high();
+     //         led_four.set_low();           
+     //   }
+        delay.delay_ms(200u8);
+     //   column_two.set_low()  ;    // set column 2 to low
+     //   column_three.set_high();   // set column 3 to high
+     //   if input_one.is_high() {                      // input 1 high button 3
+             led_one.set_high();
+              led_two.set_high();
+              led_three.set_low();
+              led_four.set_low();
+     //   } else if input_two.is_high() {               // input 2  button 8
+     //        led_four.set_high();
+     //         led_one.set_low();
+     //         led_two.set_low();
+     //         led_three.set_low();
+     //   }
+        delay.delay_ms(200u8);
+     //   column_three.set_low();    // set column 3 to low
+     //   column_four.set_high();    // set column 4 to high
+     //   if input_one.is_high() {                      // input 1 high button 4
+             led_three.set_high();
+              led_one.set_low();
+              led_two.set_low();
+              led_four.set_low();
+     //   } else if input_two.is_high() {               // input 2 high button 9
+     //        led_three.set_high();
+     //        led_one.set_high();
+     //        led_two.set_low();
+     //        led_four.set_low();
+     //   }
+        delay.delay_ms(200u8);
+     //   column_four.set_low();     // set column 4 to low
+     //   column_five.set_high();    // set column 5 to high
+     //   if input_one.is_high() {                      // input 1 high button 5
+              led_one.set_high();   
+             led_three.set_high();
+              led_two.set_low();
+              led_four.set_low();
+     //   } else if input_two.is_high() {               // input 2 high button 10
+     //         led_two.set_high();
+     //        led_four.set_high();
+     //         led_one.set_low();
+     //         led_three.set_low();
+     //   }
+        delay.delay_ms(200u8);
+     //   column_five.set_low();     // set column 5 to low
+     }
 }
